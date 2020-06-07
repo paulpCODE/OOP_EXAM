bd8ca272db88b6784062108b641e20d6566933af
Dunkeshon <andrei.ioffe@gmail.com>
2020-06-08 00:54:43 +0300

    deep copy

--- includes/linkedlists.h
+++ includes/linkedlists.h
@@ -14,95 +14,160 @@ public:
     ValueT *value;
     Node *pNext;
     Node():key(KeyT{}),value(nullptr),pNext(nullptr){}
+    Node(KeyT newKey, ValueT *newValue):key(newKey),value(newValue),pNext(nullptr){}
+    // assumes m_data is initialized
+    void deepCopy(const Node& source)
+    {
+        // first we need to deallocate any value that we are holding!
+        delete value;
+        delete pNext;
+
+        // because key is not a pointer, we can shallow copy it
+        key = source.key;
+
+
+        // value and pNext are pointers, so we need to deep copy it if it is non-null
+
+        // value
+        if (source.value)
+        {
+            // allocate memory for our copy
+            value = new ValueT();
+
+            // do the copy
+            value = source.value;
+        }
+        else
+            value = nullptr;
+
+        // pNext
+        if (source.pNext)
+        {
+            // allocate memory for our copy
+            pNext = new Node();
+
+            // do the copy
+            pNext = source.pNext;
+        }
+        else
+            pNext = nullptr;
+
+    }
+
+    // Copy constructor
+    Node(const Node& source):
+        m_data{ nullptr }
+    {
+        deepCopy(source);
+    }
+    // Assignment operator
+   Node &operator=(const Node & source)
+    {
+        // check for self-assignment
+        if (this == &source)
+            return *this;
+
+        // now do the deep copy
+        deepCopy(source);
+
+        return *this;
+    }
 };
 
 template <class KeyT,class ValueT>
 class AbstractList{
 
 public:
+    virtual void pop_front() = 0;
 
+    virtual void push_back(KeyT key,ValueT* value) = 0;
 
+    virtual void clear() = 0;
+
+    virtual int GetSize() = 0;
+
+    virtual ValueT & operator[](const int index) = 0;
+
+    virtual void push_front(KeyT key,ValueT* value) = 0;
+
+    virtual void insert(KeyT key,ValueT* value, int index) = 0;
+
+    virtual void removeAt(int index) = 0;
+
+    virtual void pop_back() = 0;
+
+    virtual void insertionSort() = 0;
 
 };
 
-template <class KeyT,class ValueT>
-class DoubleLinkedList:public AbstractList<KeyT,ValueT>
-{
-private:
-
-public:
-    DoubleLinkedList();
-    ~DoubleLinkedList();
-
-};
 
 template<class KeyT , class ValueT >
-class Linked_List
+class Linked_List : public AbstractList<KeyT,ValueT>
 {
 public:
     /** Default constructor */
-    Linked_List();
+    Linked_List() ;
 
-    Linked_List(vector<KeyT> keys, vector<ValueT> values);
+    Linked_List(const vector<KeyT>& keys,const vector<ValueT*>& values) ;
 
     // Óäàëåíèå ïåðâîãî ýëåìåíòà â ñïèñêå
-    void pop_front();
+    void pop_front() override ;
 
     // Äîáàâëåíèå ýëåìåíòà â êîíåö ñïèñêà
-    void push_back(KeyT key,ValueT value);
+    void push_back(KeyT key,ValueT* value) override ;
 
     // Î÷èñòèòü ñïèñîê
-    void clear();
+    void clear() override ;
 
     // Ïîëó÷èòü êîëè÷åñòâî åëåìåíòîâ â ñïèñêå
-    int GetSize() { return Size; }
+    int GetSize() override { return Size; }
 
     // Ïåðåãðóæåííûé îïåðàòîð []
-    T& operator[](const int index);
+    ValueT & operator[](const int index) override;
 
     // Äîáàâëåíèå ýëåìåíòà â íà÷àëî ñïèñêà
-    void push_front(KeyT key,ValueT value);
+    void push_front(KeyT key,ValueT* value) override ;
 
     // Äîáàâëåíèå ýëåìåíòà â ñïèñîê ïî óêàçàííîìó èíäåêñó
-    void insert(KeyT key,ValueT value, int index);
+    void insert(KeyT key,ValueT* value, int index) override ;
 
     // Óäàëåíèå ýëåìåíòà â ñïèñêå ïî óêàçàííîìó èíäåêñó
-    void removeAt(int index);
+    void removeAt(int index) override ;
 
     // Óäàëåíèå ïîñëåäíåãî ýëåìåíòà â ñïèñêå
-    void pop_back();
+    void pop_back() override;
 
     // Ñîðòèðîâêà 1)
-    void insertionSort();
+    void insertionSort() override ;
 
 private:
-
-    // Øàáëîííûé êëàññ Íîäû
-
-
-    int Size; // Ðàçìåð ñïèñêà
-    Node<T> *head; // Óêàçàòåëü íà ãîëîâó ñïèñêà
+    int Size;
+    Node<KeyT, ValueT> *head;
 };
 
-template<typename T>
-Linked_List<T>::Linked_List()
+template <class KeyT,class ValueT>
+Linked_List<KeyT,ValueT>::Linked_List()
 {
     Size = 0;
     head = nullptr;
 }
 
-template<typename T>
-Linked_List<T>::Linked_List(vector<T> l)
+template <class KeyT,class ValueT>
+Linked_List<KeyT,ValueT>::Linked_List(const vector<KeyT>& keys,const vector<ValueT*>& values)
 {
-    for (int i = 0; i < l.size(); i++) {
-        this->push_back(l[i]);
+    if (keys.size()!=values.size()){
+        std::cout<<"different amount of keys and values";
+        exit(0); // it'll be better if implement try-catch or assert
+    }
+    for (int i = 0; i < keys.size(); i++) {
+        this->push_back(keys[i],values[i]);
     }
 }
 
-template<typename T>
-void Linked_List<T>::pop_front()
+template <class KeyT,class ValueT>
+void Linked_List<KeyT,ValueT>::pop_front()
 {
-    Node<T> *temp = head;
+    Node<KeyT,ValueT> *temp = head;
 
     head = head->pNext;
 
@@ -112,29 +177,29 @@ void Linked_List<T>::pop_front()
 
 }
 
-template<typename T>
-void Linked_List<T>::push_back(T data)
+template <class KeyT,class ValueT>
+void Linked_List<KeyT,ValueT>::push_back(KeyT key,ValueT* value)
 {
     if (head == nullptr)
     {
-        head = new Node<T>(data);
+        head = new Node<KeyT,ValueT>(key,value);
     }
     else
     {
-        Node<T> *current = this->head;
+        Node<KeyT,ValueT> *current = this->head;
 
         while (current->pNext != nullptr)
         {
             current = current->pNext;
         }
-        current->pNext = new Node<T>(data);
+        current->pNext = new Node<KeyT,ValueT>(key,&value);
 
     }
     Size++;
 }
 
-template<typename T>
-void Linked_List<T>::clear()
+template <class KeyT,class ValueT>
+void Linked_List<KeyT,ValueT>::clear()
 {
     while (Size)
     {
@@ -144,7 +209,7 @@ void Linked_List<T>::clear()
 
 
 template<class KeyT, class ValueT>
-<KeyT,ValueT> & Linked_List<KeyT,ValueT>::operator[](const int index)
+ValueT & Linked_List<KeyT,ValueT>::operator[](const int index)
 {
     int counter = 0;
 
@@ -161,15 +226,14 @@ template<class KeyT, class ValueT>
     }
 }
 
-template<typename T>
-void Linked_List<T>::push_front(T data)
+template <class KeyT,class ValueT>
+void Linked_List<KeyT,ValueT>::push_front(T data)
 {
     head = new Node<T>(data, head);
     Size++;
 }
-
-template<typename T>
-void Linked_List<T>::insert(T data, int index)
+template <class KeyT,class ValueT>
+void Linked_List<KeyT,ValueT>::insert(T data, int index)
 {
     if (index == 0)
     {
@@ -192,8 +256,8 @@ void Linked_List<T>::insert(T data, int index)
     }
 }
 
-template<typename T>
-void Linked_List<T>::removeAt(int index)
+template <class KeyT,class ValueT>
+void Linked_List<KeyT,ValueT>::removeAt(int index)
 {
     if (index == 0)
     {
@@ -219,14 +283,14 @@ void Linked_List<T>::removeAt(int index)
 
 }
 
-template<typename T>
-void Linked_List<T>::pop_back()
+template <class KeyT,class ValueT>
+void Linked_List<KeyT,ValueT>::pop_back()
 {
     removeAt(Size - 1);
 }
 
-template<typename T>
-void Linked_List<T>::insertionSort()
+template <class KeyT,class ValueT>
+void Linked_List<KeyT,ValueT>::insertionSort()
 {
     int i, j;
     DateTime key;

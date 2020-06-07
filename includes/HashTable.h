5128e880ac85f84dd547efc48d999236943a72ce
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-06-08 00:55:40 +0300

    Add the 'size' data member (HashTable.h)

--- includes/HashTable.h
+++ includes/HashTable.h
@@ -1,10 +1,16 @@
 #pragma once
 
 #include "HashNode.h"
+#include <cstddef>
 
 template <typename KeyType, typename DataType>
 class HashTable {
+protected:
+	std::size_t size;
+
 public:
+	HashTable(std::size_t size) : size{ size } {}
+
 	virtual void insert(KeyType key, DataType* data) = 0;
 	virtual void remove(KeyType key) = 0;
 	virtual HashNode<KeyType, DataType>* search(KeyType key) = 0;

1802aeac0f41bd3c27b7b6138de4d01f906ba5b9
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-06-10 01:22:43 +0300

    Implement union, intersection and difference operations on set (Set.h)

--- includes/Set.h
+++ includes/Set.h
@@ -23,9 +23,41 @@ public:
 	virtual bool contains(KeyType key) = 0;
 	virtual void remove(KeyType key) = 0;
 
+	virtual void clear() = 0;
+
 	virtual std::vector<KeyType> getAllKeys() = 0;
 	virtual std::vector<DataType*> getAllData() = 0;
 	virtual std::vector<std::pair<KeyType, DataType*>> getAllKeysData() = 0;
+
+	virtual void unionise(Set<KeyType, DataType>& other) {
+		std::vector<std::pair<KeyType, DataType*>> pairs = other.getAllKeysData();
+		for (auto& pair : pairs) {
+			if (!contains(pair.first)) {
+				insert(pair.first, pair.second);
+			}
+		}
+	}
+
+	virtual void intersection(Set<KeyType, DataType>& other) {
+		std::vector<std::pair<KeyType, DataType*>> pairs = other.getAllKeysData();
+		std::vector<std::pair<KeyType, DataType*>> commonPairs;
+		for (auto& pair : pairs) {
+			if (contains(pair.first)) {
+				pairs.emplace_back(std::pair<KeyType, DataType*>{pair.first, pair.second});
+			}
+		}
+		clear();
+		for (auto& pair : commonPairs) {
+			insert(pair.first, pair.second);
+		}
+	}
+
+	virtual void difference(Set<KeyType, DataType>& other) {
+		std::vector<std::pair<KeyType, DataType*>> pairs = other.getAllKeysData();
+		for (auto& pair : pairs) {
+			remove(pair.first);
+		}
+	}
 
 };
 
@@ -56,6 +88,8 @@ public:
 	bool contains(KeyType key) override { return table->search(key); }
 	void remove(KeyType key) override { table->remove(key); }
 
+	void clear() override { table->clear(); };
+
 	std::vector<KeyType> getAllKeys() override { return table->getAllKeys(); }
 	std::vector<DataType*> getAllData() override { return table->getAllData(); }
 	std::vector<std::pair<KeyType, DataType*>> getAllKeysData() override { return table->getAllKeysData(); }
@@ -74,6 +108,8 @@ public:
 	void insert(KeyType key, DataType* data) override { list->push_back(key, data); }
 	bool contains(KeyType key) override { return list->search(key); }
 	void remove(KeyType key) override { list->remove(key); }
+
+	void clear() override { list->clear(); };
 
 	std::vector<KeyType> getAllKeys() override { return list->getAllKeys(); }
 	std::vector<DataType*> getAllData() override { return list->getAllData(); }
@@ -107,6 +143,8 @@ public:
 	void insert(KeyType key, DataType* data) override { tree->insert(key, data); }
 	bool contains(KeyType key) override { return tree->search(key); }
 	void remove(KeyType key) override { tree->remove(key); }
+
+	void clear() override { tree->clear(); };
 
 	std::vector<KeyType> getAllKeys() override { return tree->getAllKeys(); }
 	std::vector<DataType*> getAllData() override { return tree->getAllData(); }
